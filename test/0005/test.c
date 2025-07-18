7df9e9fedafa9bbabed887b4c149ee72f7954840
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2025-07-18 15:08:55 +0200

    Test of db w/ table.

---
+++ test/0005/test.c
@@ -0,0 +1,143 @@
+#include <string.h>
+#include <stdio.h>
+#include <util/table_t.h>
+#include <sdbm_tree/td.h>
+#include <fcntl.h>
+#include <time.h>
+
+static
+char* fakeword
+  (char in[ 16 ])
+{
+  unsigned l = (rand() % 8) + 2;
+  for (unsigned i=0; i < l; i++) {
+    in[ i ] = 'a' + (rand() % 26);
+    in[ i+1 ] = 0;
+  }
+  return in;
+}
+
+int main
+  (int argc, char* argv[])
+{
+  (void)argc;
+  (void)argv;
+
+  srand(time(0));
+
+  td_t td = { 0 };
+
+  td_open(&td, "/tmp/db.db", TDFLG_ALLOCTDT, O_RDWR|O_CREAT|O_TRUNC, 0644);
+
+  {
+    if (table_insert_row(&td, "users", 3,
+          "user_name", DB_FIELDTYPE_STRING, "Katherine",
+          "user_email", DB_FIELDTYPE_STRING, "foo@bar.com",
+          "user_age", DB_FIELDTYPE_INTEGER, 32
+    ))
+    {
+      fprintf(stderr, "FAILURE (%s:%d).\n", __FILE__, __LINE__);
+      return ~0;
+    }
+  }
+  fprintf(stderr, "Subtest Ok.\n");
+
+  {
+    unsigned l = 0;
+    if (table_get_size(&td, "users", &l)) {
+      fprintf(stderr, "FAILURE (%s:%d).\n", __FILE__, __LINE__);
+      return ~0;
+    }
+    if (l != 1) {
+      fprintf(stderr, "FAILURE (%s:%d).\n", __FILE__, __LINE__);
+      return ~0;
+    }
+  }
+  fprintf(stderr, "Subtest Ok.\n");
+
+  {
+    for (unsigned i=0; i < 128; i++) {
+      if (table_insert_row(&td, "users", 3,
+            "user_name", DB_FIELDTYPE_STRING, "Katherine",
+            "user_email", DB_FIELDTYPE_STRING, "foo@bar.com",
+            "user_age", DB_FIELDTYPE_INTEGER, 32
+      ))
+      {
+        fprintf(stderr, "FAILURE (%s:%d).\n", __FILE__, __LINE__);
+        return ~0;
+      }
+    }
+  }
+  fprintf(stderr, "Subtest Ok.\n");
+
+  {
+    unsigned l = 0;
+    if (table_get_size(&td, "users", &l)) {
+      fprintf(stderr, "FAILURE (%s:%d).\n", __FILE__, __LINE__);
+      return ~0;
+    }
+    if (l != 129) {
+      fprintf(stderr, "FAILURE (%s:%d).\n", __FILE__, __LINE__);
+      return ~0;
+    }
+  }
+  fprintf(stderr, "Subtest Ok.\n");
+
+  {
+    for (unsigned i=0; i < 127; i++) {
+      if (table_delete_row(&td, "users", (uint64_t)(i+1))) {
+        fprintf(stderr, "FAILURE (%s:%d).\n", __FILE__, __LINE__);
+        return ~0;
+      }
+    }
+  }
+  fprintf(stderr, "Subtest Ok.\n");
+
+  {
+    unsigned l = 0;
+    if (table_get_size(&td, "users", &l)) {
+      fprintf(stderr, "FAILURE (%s:%d).\n", __FILE__, __LINE__);
+      return ~0;
+    }
+    if (l != 2) {
+      fprintf(stderr, "FAILURE (%s:%d).\n", __FILE__, __LINE__);
+      return ~0;
+    }
+  }
+  fprintf(stderr, "Subtest Ok.\n");
+
+  {
+    char words[ 2 ][ 16 ];
+    for (unsigned i=0; i < 128; i++) {
+      if (table_insert_row(&td, "users", 3,
+            "user_name", DB_FIELDTYPE_STRING, fakeword(words[ 0 ]),
+            "user_email", DB_FIELDTYPE_STRING, fakeword(words[ 1 ]),
+            "user_age", DB_FIELDTYPE_INTEGER, (rand() % 70)
+      ))
+      {
+        fprintf(stderr, "FAILURE (%s:%d).\n", __FILE__, __LINE__);
+        return ~0;
+      }
+    }
+  }
+  fprintf(stderr, "Subtest Ok.\n");
+
+  {
+    row_t row = { 0 };
+    if (table_get_row(&td, "users", 200, &row)) {
+      fprintf(stderr, "FAILURE (%s:%d).\n", __FILE__, __LINE__);
+      return ~0;
+    }
+    for (unsigned i=0; i < row.fields.count; i++) {
+      fprintf(stderr, "Row %s : %s\n"
+                      , row.fields.list[ i ].name
+                      , (char*)row.fields.list[ i ].value.data);
+    }
+    fprintf(stderr, "Row username: %s\n", row_get_as_string(&row, "user_name"));
+  }
+  fprintf(stderr, "Subtest Ok.\n");
+
+  fprintf(stderr, "SUCCESS.\n");
+
+  return 0;
+}
